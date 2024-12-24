ef4384cb30f2b959ae87a7ba22a99b7412e0c308
its-meet0123 <sukh.meet9896@gmail.com>
2024-12-24 09:38:29 +0530

    udated

--- src/Components/Pizzamenu.js
+++ src/Components/Pizzamenu.js
@@ -4,22 +4,34 @@ import "./Pizzamenu.css";
 import { pizzaData } from "./Pizzadata.js";
 
 function Menu() {
+  const pizzas = pizzaData;
+  //const pizzas = [];
+  const numPizzas = pizzas.length;
   return (
-    <container-fluid className="pizza-bar">
-      <h1 className="text-warning text-center">RAMGHRIA PIZZA HOUSE</h1>
-      
-      <main className="menu">
-      <div className="menu-data">
-        <h2 className="text-center" id='our-menu'>OUR MENU</h2>
-        <ul className="menu-list">
-          {pizzaData.map((pizza) => (
-             <Pizza pizzaObj = {pizza} key={pizza.id} />
-          ))}
-        </ul>
-        </div>
-      </main>
-      
-    </container-fluid>
+    <><container-fluid className="pizza-bar">
+      <h1 className="text-warning " id="name-pizzabar">RAMGHRIA PIZZA HOUSE</h1>
+      <div className="values">
+        <main className="menu">
+          <div className="menu-data">
+           <h2 className="text-center" id='our-menu'>OUR MENU</h2>
+           {/* <ul className="menu-list">
+           {pizzaData.map((pizza) => (
+           <Pizza pizzaObj = {pizza} key={pizza.id} />
+           ))}
+           </ul> */}
+            {numPizzas > 0 && (
+            <ul className="menu-list">
+              {pizzas.map((pizza) => (
+                <Pizza pizzaObj={pizza} key={pizza.name} />
+              ))}
+            </ul>
+           )}
+          </div>
+        </main>
+        <Footer />
+      </div>
+     </container-fluid>
+    </>
   );
 }
 export { Menu };
@@ -39,4 +51,22 @@ function Pizza(props) {
       </div>
     </li>
   );
+}
+
+function Footer(){
+  const hour = new Date().getHours();
+  const openHour = 9;
+  const closeHour = 24;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  return(
+    <footer className="footer">{isOpen && 
+    <div className="order">
+      <p className="text-secondary" id="name">We're <b>open</b> until {closeHour}:00.</p>
+      <button type="button" className="btn btn-warning">Order</button>
+      </div>
+      }
+      </footer>
+  )
 }
